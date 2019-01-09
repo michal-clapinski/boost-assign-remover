13829ff3566cef300dcb9a9acef57620fdc9a134
Michał Cłapiński <michal.clapinski@codilime.com>
2019-01-09 20:23:30 +0000

    basic support for multiline list_of

--- parser.py
+++ parser.py
@@ -6,8 +6,11 @@ with open(path, "r") as f:
     lines = f.read().splitlines()
 
 l2 = []
-for i in range(len(lines)):
-    if "list_of" in lines[i] and ";" in lines[i]:
+
+i = 0
+while i < len(lines):
+    oi = i
+    if "list_of" in lines[i] and "#include" not in lines[i]:
         l2.append("#if __cplusplus >= 201103L")
         pos = lines[i].find("list_of")
         temp = lines[i][0:pos]
@@ -17,17 +20,26 @@ for i in range(len(lines)):
             p3 = lines[i][p2:].find(")")
             temp += lines[i][p2:p2+p3]
             p2 += p3 + 2
-            if len(lines[i]) == p2:
+            if len(lines[i]) == p2 - 1:
+                i += 1
+                temp += ","
+                l2.append(temp)
+                p2 = lines[i].find("(") + 1
+                temp = lines[i][:p2-1]
+            elif len(lines[i]) == p2 and lines[i][p2-1] == ";":
                 break
             else:
                 temp += ", "
         temp += "};"
         l2.append(temp)
         l2.append("#else")
-        l2.append(lines[i])
+        while oi <= i:
+            l2.append(lines[oi])
+            oi += 1
         l2.append("#endif")
     else:
         l2.append(lines[i])
+    i += 1
 
 with open(path, "w") as f:
     for line in l2:
